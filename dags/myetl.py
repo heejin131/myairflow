a3374efb502a3133bc160a5bf508cc8529ce2ecb
heejin131 <journeyjean131@gmail.com>
2025-03-13 15:11:24 +0900

    repo 생성 예정

--- dags/myetl.py
+++ dags/myetl.py
@@ -19,13 +19,15 @@ with DAG(
         df = pd.read_csv(f"/home/gmlwls5168/data/{timestamp}/data.csv")
         df.to_parquet(f"/home/gmlwls5168/data/{timestamp}/data.parquet", engine='pyarrow')
         df_loaded = pd.read_parquet('data.parquet', engine='pyarrow')
-        print(df_loaded)  
+        print(df_loaded)
     
     def agg_data_py(timestamp):
         import pandas as pd
+        import pendulum
         df = pd.read_parquet(f"/home/gmlwls5168/data/{timestamp}/data.parquet")
         agg_df = df.groupby(['name', 'value']).size().reset_index(name='count')
         agg_df.to_csv(f'/home/gmlwls5168/data/{timestamp}/agg.csv', index=False)
+        agg_df['timestamp'] = pendulum.now('Asia/Seoul').format('YYYY-MM-DDTHH:mm:ss.SSSSSS[Z]')
         
     make_data=BashOperator(task_id="make", bash_command="bash /home/gmlwls5168/airflow/make_data.sh /home/gmlwls5168/data/{{data_interval_start.in_tz('Asia/Seoul').format('YYYY/MM/DD/HH')}}")
     load_data=PythonVirtualenvOperator(task_id="load", 
@@ -34,7 +36,7 @@ with DAG(
                                        op_args=["{{ data_interval_start.in_tz('Asia/Seoul').format('YYYY/MM/DD/HH')}}"])
     agg_data=PythonVirtualenvOperator(task_id="agg",
                                       python_callable=agg_data_py,
-                                      requirements=["pandas","pyarrow"],
+                                      requirements=["pandas","pyarrow","pendulum"],
                                       op_args=["{{ data_interval_start.in_tz('Asia/Seoul').format('YYYY/MM/DD/HH')}}"]
                                       )
 
