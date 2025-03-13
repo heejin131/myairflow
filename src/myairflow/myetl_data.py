95831c70c636a627ded3fbbd746bc8d843a7b167
heejin131 <journeyjean131@gmail.com>
2025-03-13 15:12:05 +0900

    repo 생성예정

--- src/myairflow/myetl_data.py
+++ src/myairflow/myetl_data.py
@@ -32,3 +32,17 @@ def converter_pq(dis_path):
         return f"Parquet 파일이 아래 주소에 생성되었습니다: {parquet_file}"
     except Exception:
         return "파일 생성 중 오류가 발생했습니다"
+
+
+
+def load_data_py(timestamp):
+    df = pd.read_csv(f"/home/gmlwls5168/data/{timestamp}/data.csv")
+    df.to_parquet(f"/home/gmlwls5168/data/{timestamp}/data.parquet", engine='pyarrow')
+    df_loaded = pd.read_parquet('data.parquet', engine='pyarrow')
+    print(df_loaded)  
+  
+def agg_data_py(timestamp):
+    df = pd.read_parquet(f"/home/gmlwls5168/data/{timestamp}/data.parquet")
+    agg_df = df.groupby(['name', 'value']).size().reset_index(name='count')
+    agg_df.to_csv(f'/home/gmlwls5168/data/{timestamp}/agg.csv', index=False)
+    agg_df['timestamp'] = datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
