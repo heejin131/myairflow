ab32f20b97809457ed3d4c9acb5b72e67c79bc04
heejin131 <journeyjean131@gmail.com>
2025-03-20 17:15:59 +0900

    meta & merge

---
+++ dags/movie_after.py
@@ -0,0 +1,98 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import BranchPythonOperator, PythonVirtualenvOperator
+from airflow.sensors.filesystem import FileSensor
+
+DAG_ID = "movie_after"
+
+with DAG(
+    DAG_ID,
+    default_args={
+        'depends_on_past': True,
+        'retries': 1,
+        'retry_delay': timedelta(seconds=3),
+    },
+    max_active_runs=1,
+    max_active_tasks=5,
+    description='movie',
+    schedule="10 10 * * *",
+    start_date=datetime(2024, 1, 1),
+    end_date=datetime(2024, 1, 5),
+    catchup=True,
+    tags=['api', 'movie'],
+) as dag:
+    REQUIREMENTS = ["git+https://github.com/heejin131/movie.git@0.3.0"]
+    BASE_DIR = f"~/data/{DAG_ID}"
+
+    start = EmptyOperator(task_id='start')
+    end = EmptyOperator(task_id = 'end')
+
+    #경로에 파일이 존재하는 지 확인하는 작업
+    check_done = FileSensor(
+        task_id="check.done",
+        filepath="/home/gmlwls5168/data/movies/done/dailyboxoffice/{{ds_nodash}}/_DONE",
+        fs_conn_id="fs_after_movie",
+        poke_interval=180,  # 3분마다 체크
+        timeout=3600,  # 1시간 후 타임아웃
+        mode="reschedule",  # 리소스를 점유하지 않고 절약하는 방식
+    )
+
+    #경로에 파일을 저장하는 작업
+    def fn_gen_meta(**kwargs):
+        import json
+        import pandas as pd
+        from movie.api.call import fillna_meta
+        print(json.dumps(kwargs, indent=4, ensure_ascii=False))
+        meta_df = fillna_meta(previous_df, current_df)
+        current_df = pd.read_parquet('/home/gmlwls5168/data/movies/dailyboxoffice/dt={ds_nodash}')
+        save_path = "/home/gmlwls5168/data/movie_after/meta/meta.parquet"
+        meta_df.to_parquet(save_path)
+        print(meta_df)
+
+    gen_meta= PythonVirtualenvOperator(
+        task_id='gen.meta',
+        python_callable=fn_gen_meta,
+        system_site_packages=False,
+        requirements=REQUIREMENTS
+    )
+
+    def fn_gen_movie(base_path, ds_nodash, **kwargs):
+        import json
+        from movie.api.call import fillna_meta
+        print(json.dumps(kwargs, indent=4, ensure_ascii=False))
+        print(f"base_path: {base_path}")
+        
+        # TODO -> f"{base_path}/dailyboxoffice/ 생성
+        meta_df = fillna_meta(previous_df, current_df)
+        save_path = "/home/gmlwls5168/data/movie_after/dailyoffice/dt={ds_nodash}"
+        previous_df = pd.read_parquet("/home/gmlwls5168/data/movie_after/meta/meta.parquet")
+        current_df = pd.read_parquet('/home/gmlwls5168/data/movies/dailyboxoffice/dt={ds_nodash}')
+        meta_df.to_parquet(meta_save_path, partition_cols=['dt', 'multiMovieYn', 'repNationCd'])
+        meta_df.to_parquet(save_path)
+        print("fin")
+
+
+
+    gen_movie = PythonVirtualenvOperator(
+        task_id="gen.movie",
+        python_callable=fn_gen_movie,
+        requirements=REQUIREMENTS,
+        system_site_packages=False,
+        op_kwargs={"base_path": BASE_DIR},
+    )
+
+    make_done = BashOperator(
+        task_id='make.done',
+        bash_command="""
+        DONE_BASE = $BASE_DIR/done
+        echo $DONE_BASE
+        mkdir -p $DONE_BASE/{{ ds_nodash }}
+        touch $DONE_BASE/{{ ds_nodash }}/_DONE
+        """,
+        env={"BASE_DIR":BASE_DIR},
+        append_env = True
+    )
+
+    start >> check_done >> gen_meta >> gen_movie >> make_done >> end
